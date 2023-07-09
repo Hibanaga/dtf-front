2637600de5dc049053c8fa4041f605fb2ca88766
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2023-07-09 11:22:43 +0200

    DTF-1 Implement custom hook to handle situation click outside search bar

--- src/components/modules/SearchBar/component.tsx
+++ src/components/modules/SearchBar/component.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import React, { FunctionComponent, useEffect, useRef, useState } from 'react';
 import EditNoteRoundedIcon from '@mui/icons-material/EditNoteRounded';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 
@@ -12,17 +12,25 @@ import { getItem, setItem } from 'utils/localStorage';
 import Button from 'components/layout/Button';
 import Input from 'components/layout/forms/SimpleInput';
 import Popover from 'components/layout/Popover';
+import { useOnClickOutside } from 'hooks/useOnClickOutside';
 
 import { Props } from './index';
 import StyledComponent from './styles';
 
-
 //TODO: after integrate API for search on system add redirect to click on element popover
 //TODO: handle situation when click to recent element, popover must save this element to searchbar, but now this doesn't work properly
 const ModuleSearchBar: FunctionComponent<Props> = ({ children }) => {
+    const ref = useRef(null);
+
     const [search, setSearch] = useState('');
     const [isFocusedInput, setIsFocusedInput] = useState(false);
     const [recentSearchItems, setRecentSearchItems] = useState<Option<string>[]>([]);
+
+    const handleClickOutside = () => {
+        setIsFocusedInput(false);
+    };
+
+    useOnClickOutside(ref, handleClickOutside);
 
     const showRecentQuestionsPopover = isFocusedInput && !search?.length && !!recentSearchItems?.length;
     const showSearchListPopover = isFocusedInput && !!search?.length;
@@ -45,11 +53,13 @@ const ModuleSearchBar: FunctionComponent<Props> = ({ children }) => {
 
     return (
         <StyledComponent className="module-search-bar">
-            <div className="smart-search-bar">
+            <div
+                ref={ref}
+                className="smart-search-bar"
+            >
                 <Input
                     placeholder="Пошук"
                     onFocus={() => setIsFocusedInput(true)}
-                    onBlur={() => setIsFocusedInput(false)}
                     onKeyDown={handleDetectKeydown}
                     value={search}
                     onChange={(e) => setSearch(e.target.value)}
