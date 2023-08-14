5af324b086ef834f7b2d46ccb9a61ba40a7b7941
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2023-08-14 10:30:40 +0200

    DTF-4 Modify initial sturcture

--- src/components/layout/ListPaginatedElement/component.tsx
+++ src/components/layout/ListPaginatedElement/component.tsx
@@ -6,6 +6,16 @@ import StyledComponent from './styles';
 const ListPaginatedElement: FunctionComponent<Props> = ({ }) => {
     return (
         <StyledComponent className="layout-paginated-element">
+            <div className="header-inner">
+                <div className="data-category">
+                    <span className="data-value"></span>
+                </div>
+                <div className="data-publication-time">
+                    <span className="data-value"></span>
+                </div>
+            </div>
+            <div className="body-innter"></div>
+            <div className="footer-inner"></div>
         </StyledComponent>
     );
 };
